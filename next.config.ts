05eaccf3d2327e90617be8e847d3156ecdc94b40
Yuga Mauludi <yugamauludi98@gmail.com>
2025-05-16 17:19:51 +0700

    feat(camera): add camera feed page with live stream and image history

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/v01/occ/api/:path*`,
-      },
-    ];
+    return {
+      fallback: [
+        {
+          source: '/api/:path*',
+          destination: `${process.env.NEXT_PUBLIC_API_URL}/v01/occ/api/:path*`,
+        },
+      ],
+    };
   },
 };
 
