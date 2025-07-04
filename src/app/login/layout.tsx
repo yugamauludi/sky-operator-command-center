e1cd5394b64f76cf424838c1c49068fb86dd7b0d
Yuga Mauludi <yugamauludi98@gmail.com>
2025-07-04 16:03:22 +0700

    refactor(login): remove commented-out auth code from layout

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,7 +1,30 @@
+// import { cookies } from "next/headers";
+// import { redirect } from "next/navigation";
+// import jwt from "jsonwebtoken";
+
 export default async function LoginLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // const cookieStore = await cookies();
+  // const token = cookieStore.get("token")?.value;
+
+  // if (token) {
+  //   let isValid = false;
+
+  //   try {
+  //     jwt.verify(token, process.env.JWT_SECRET!);
+  //     isValid = true;
+  //   } catch (err) {
+  //     console.error("Invalid token:", err);
+  //   }
+
+  //   if (isValid) {
+  //     redirect("/"); // ⬅️ letakkan di luar try-catch
+  //   }
+  // }
+
+  // ❌ token tidak ada atau tidak valid
   return <div>{children}</div>;
 }
